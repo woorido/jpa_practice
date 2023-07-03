f7dda8934a8200655597287ec73e4d1c8a64f5fd
PiHyoJeong <shson4007@gmail.com>
2023-07-03 18:22:34 +0900

    Feat: 회원등록

---
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.controller;
+
+
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+
+@Setter
+public class MemberForm {
+
+    @NotEmpty(message = "회원 이름은 필수 입니다")
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+}
