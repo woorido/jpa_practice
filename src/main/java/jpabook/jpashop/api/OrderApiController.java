0d2d31f80e106bb3d925f11ba22b7827817270aa
PiHyoJeong <shson4007@gmail.com>
2023-07-11 17:41:02 +0900

    Feat: 엔티티 직접 노출하여 주문 조회 (entity 를 직접 노출하면 안됨)

---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> orders = orderRepository.findAll(new OrderSearch());
+        for (Order order : orders) {
+            order.getMember().getName(); //강제초기화
+            order.getDelivery().getAddress(); //강제초기화
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName()); //강제초기화
+        }
+        return orders;
+    }
+}
